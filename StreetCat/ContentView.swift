37e1840883212c6b38ef3f5dc246899ced557ced
Changnam Hong <hcn1519@gmail.com>
2019-10-17 21:59:26 +0900

    First Commit

---
+++ StreetCat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StreetCat
+//
+//  Created by 홍창남 on 2019/10/17.
+//  Copyright © 2019 depromeet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
