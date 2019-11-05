ba6ef9298850b70a05b98fec8a5697f49272ceca
Changnam <hcn1519@gmail.com>
2019-11-06 06:44:22 +0900

    [Animation] MiniFloatingView 에니메이션 추가

--- StreetCat/MapViewController.swift
+++ StreetCat/MapViewController.swift
@@ -19,16 +19,16 @@ class MapViewController: UIViewController {
     // TODO: SwiftUI 작업
     let floatingPanelViewController = UIViewController()
 
+    let miniFloatingView = MiniFloatingView(frame: .zero)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let mapView = NMFMapView(frame: view.frame)
         view.addSubview(mapView)
 
+        view.addSubview(miniFloatingView)
         view.addSubview(controlPanelContainerView)
-
-        addChild(floatingPanelViewController)
-        view.addSubview(floatingPanelViewController.view)
 
         controlPanelContainerView.snp.makeConstraints {
             $0.height.equalTo(70)
@@ -37,17 +37,42 @@ class MapViewController: UIViewController {
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
         }
 
-        controlPanelContainerView.controlPanelView.didTapAroundButton = { [weak self] _ in
-            self?.floatingPanelViewController.view.isHidden = false
+        controlPanelContainerView.controlPanelView.didTapFindDirectionButton = { [weak self] sender in
+            DispatchQueue.main.async {
+                self?.tapControlPanelButton(sender: sender, type: .findDirection)
+            }
+        }
+        
+        controlPanelContainerView.controlPanelView.didTapAroundButton = { [weak self] sender in
+            DispatchQueue.main.async {
+                self?.tapControlPanelButton(sender: sender, type: .around)
+            }
         }
 
-        floatingPanelViewController.view.snp.remakeConstraints {
-            $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalToSuperview()
-            $0.height.size.equalTo(300)
+        miniFloatingView.snp.makeConstraints {
+            $0.leading.equalTo(controlPanelContainerView.snp.leading)
+            $0.trailing.equalTo(controlPanelContainerView.snp.trailing)
+            $0.bottom.equalTo(controlPanelContainerView.snp.top).offset(35)
+            $0.height.equalTo(0)
         }
-        floatingPanelViewController.view.isHidden = true
-        floatingPanelViewController.view.backgroundColor = .white
+    }
+
+    private func tapControlPanelButton(sender: UIButton, type: FloatingViewType) {
+        let isSelected = !sender.isSelected
+        
+        switch type {
+        case .findDirection:
+            controlPanelContainerView.controlPanelView.findDirectionButton.isSelected = isSelected
+        case .around:
+            controlPanelContainerView.controlPanelView.aroundButton.isSelected = isSelected
+        case .none:
+            break
+        }
+
+        self.miniFloatingView.viewModel = MiniFloatingViewModel(type: type, isSelected: isSelected)
+        
+        UIView.animate(withDuration: 0.7, delay: 0.1, usingSpringWithDamping: 0.7, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
-
